5465d32a89d67eed50c5dec1f4fffe489832a328
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-12-06 14:30:19 -0600

    fix: save previous image when editing in suit_jacket_details page

--- admin_pages/suit_jacket_details.php
+++ admin_pages/suit_jacket_details.php
@@ -1,114 +1,120 @@
 <?php
-    // Database connection file
-    include '../database/connection.php';
+// Database connection file
+include '../database/connection.php';
 
-    // Variables to pre-fill the form (default values)
-    $model = '';
-    $color = '';
-    $chest_size = '';
-    $shoulder_size = '';
-    $price = '';
+// Variables to pre-fill the form (default values)
+$model = '';
+$color = '';
+$chest_size = '';
+$shoulder_size = '';
+$price = '';
 
-    // List of options for selectors
-    $model_options = [
-        'one-button' => 'One-Button',
-        'two-button' => 'Two-Button',
-        'four-button' => 'Four-Button',
-        'single-breasted' => 'Single-Breasted',
-        'double-breasted' => 'Double-Breasted',
-        'smoking' => 'Smoking',
-        'tailcoat' => 'Tailcoat',
-        'smokblazering' => 'Blazer-Sport',
-    ];
+// List of options for selectors
+$model_options = [
+    'one-button' => 'One-Button',
+    'two-button' => 'Two-Button',
+    'four-button' => 'Four-Button',
+    'single-breasted' => 'Single-Breasted',
+    'double-breasted' => 'Double-Breasted',
+    'smoking' => 'Smoking',
+    'tailcoat' => 'Tailcoat',
+    'smokblazering' => 'Blazer-Sport',
+];
 
-    $color_options = [
-        'black' => 'Negro',
-        'dark-blue' => 'Azul marino',
-        'gray' => 'Gris',
-        'dark-gray' => 'Gris oscuro',
-        'white' => 'Blanco',
-        'beige' => 'Beige',
-    ];
+$color_options = [
+    'black' => 'Negro',
+    'dark-blue' => 'Azul marino',
+    'gray' => 'Gris',
+    'dark-gray' => 'Gris oscuro',
+    'white' => 'Blanco',
+    'beige' => 'Beige',
+];
 
-    // Check if an ID is provided in the URL
-    if (isset($_GET['id'])) {
-        $edit_jacket_id = $_GET['id'];
+// Check if an ID is provided in the URL
+if (isset($_GET['id'])) {
+    $edit_jacket_id = $_GET['id'];
 
-        // Make a query to obtain the details of the bag to edit
-        $sql = "SELECT * FROM SuitJackets WHERE jacket_ID = $edit_jacket_id";
-        $result = $conn->query($sql);
+    // Make a query to obtain the details of the bag to edit
+    $sql = "SELECT * FROM SuitJackets WHERE jacket_ID = $edit_jacket_id";
+    $result = $conn->query($sql);
 
-        // Check if details were found
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
+    // Check if details were found
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
 
-            // Use the details to pre-fill the form
-            $model = $row['model'];
-            $color = $row['color'];
-            $chest_size = $row['chest_size'];
-            $shoulder_size = $row['shoulder_size'];
-            $price = $row['price'];
+        // Use the details to pre-fill the form
+        $model = $row['model'];
+        $color = $row['color'];
+        $chest_size = $row['chest_size'];
+        $shoulder_size = $row['shoulder_size'];
+        $price = $row['price'];
 
-            // Current image path
-            $img_path = $row['image_src'];
-        }
+        // Current image path
+        $img_path = $row['image_src'];
     }
+}
 
-    // Check if the form has been submitted
-    if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        // Retrieve form data
-        $model = $_POST['model'];
-        $color = $_POST['color'];
-        $chest_size = $_POST['chest_size'];
-        $shoulder_size = $_POST['shoulder_size'];
-        $price = $_POST['price'];
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Retrieve form data
+    $model = $_POST['model'];
+    $color = $_POST['color'];
+    $chest_size = $_POST['chest_size'];
+    $shoulder_size = $_POST['shoulder_size'];
+    $price = $_POST['price'];
 
-        // Image processing
-        $img_name = $_FILES['imagen']['name'];
-        $img_tmp = $_FILES['imagen']['tmp_name'];
+    // Data cleaning
+    $model = mysqli_real_escape_string($conn, $model);
+    $color = mysqli_real_escape_string($conn, $color);
+    $chest_size = mysqli_real_escape_string($conn, $chest_size);
+    $shoulder_size = mysqli_real_escape_string($conn, $shoulder_size);
+    $price = mysqli_real_escape_string($conn, $price);
 
-        // Check if a file was uploaded and if it is an image
-        if (is_uploaded_file($img_tmp) && getimagesize($img_tmp)) {
-            // Path where the image will be saved
-            $img_path = "../uploads/" . $img_name;
+    // Check if it is an update or insert
+    if (isset($_POST['edit_jacket_id'])) {
+        // Update: prepare the SQL query to update
+        $update_id = $_POST['edit_jacket_id'];
 
-            // Move the image to the specified folder
-            move_uploaded_file($img_tmp, $img_path);
+        // If a new image is uploaded, update the image path; otherwise, keep the existing one
+        if ($_FILES['imagen']['size'] > 0) {
+            $img_name = $_FILES['imagen']['name'];
+            $img_tmp = $_FILES['imagen']['tmp_name'];
 
-            // Data cleaning
-            $model = mysqli_real_escape_string($conn, $model);
-            $color = mysqli_real_escape_string($conn, $color);
-            $chest_size = mysqli_real_escape_string($conn, $chest_size);
-            $shoulder_size = mysqli_real_escape_string($conn, $shoulder_size);
-            $price = mysqli_real_escape_string($conn, $price);
-            $img_path = mysqli_real_escape_string($conn, $img_path);
+            // Check if a file was uploaded and if it is an image
+            if (is_uploaded_file($img_tmp) && getimagesize($img_tmp)) {
+                // Path where the new image will be saved
+                $img_path = "../uploads/" . $img_name;
 
-            // Check if it is an update or insert
-            if (isset($_POST['edit_jacket_id'])) {
-                // Update: prepare the SQL query to update
-                $update_id = $_POST['edit_jacket_id'];
-                $sql = "UPDATE SuitJackets SET model = '$model', color = '$color', chest_size = $chest_size, shoulder_size = $shoulder_size, price = $price, image_src = '$img_path' WHERE jacket_ID = $update_id";
-            } else {
-                // Insert: prepare the SQL query to insert
-                $sql = "INSERT INTO SuitJackets (model, color, chest_size, shoulder_size, price, image_src) VALUES ('$model', '$color', $chest_size, $shoulder_size, $price, '$img_path')";
+                // Move the new image to the specified folder
+                move_uploaded_file($img_tmp, $img_path);
             }
-
-            // Execute the query
-            if ($conn->query($sql) === TRUE) {
-                $message = isset($_POST['edit_jacket_id']) ? 'Saco editado exitosamente' : 'Saco agregado exitosamente';
-                echo "<div class='success-message'>" . $message . "</div>";
-                header("refresh:2;url=./suit_jackets.php");
-            } else {
-                echo "Error al agregar el saco: " . $conn->error;
-            }
-
-        } else {
-            echo "Error: Subida de archivo no válida o no es una imagen.";
         }
 
-        // Close the connection to the database
-        $conn->close();
+        $sql = "UPDATE SuitJackets SET model = '$model', color = '$color', chest_size = $chest_size, shoulder_size = $shoulder_size, price = $price";
+
+        // Update the image path only if a new image was uploaded
+        if (isset($img_path)) {
+            $sql .= ", image_src = '$img_path'";
+        }
+
+        $sql .= " WHERE jacket_ID = $update_id";
+    } else {
+        // Insert: prepare the SQL query to insert
+        $sql = "INSERT INTO SuitJackets (model, color, chest_size, shoulder_size, price, image_src) VALUES ('$model', '$color', $chest_size, $shoulder_size, $price, '$img_path')";
     }
+
+    // Execute the query
+    if ($conn->query($sql) === TRUE) {
+        $success_message = isset($_POST['edit_jacket_id']) ? 'Saco editado exitosamente' : 'Saco agregado exitosamente';
+        echo "<div class='success-message'>" . $success_message . "</div>";
+        header("refresh:2;url=./suit_jackets.php");
+    } else {
+        echo "Error al agregar el saco: " . $conn->error;
+    }
+
+    // Close the connection to the database
+    $conn->close();
+}
 ?>
 
 <!DOCTYPE html>
@@ -154,9 +160,14 @@
 
         <div>
             <label for="imagen"><?php echo isset($edit_jacket_id) ? 'Editar' : 'Agregar'; ?> imagen</label>
-            <input type="file" name="imagen" id="imagen_src" onchange="previewImage()" <?php echo isset($edit_jacket_id) ? '' : 'required'; ?>>
-            <img id="image-preview" src="<?php echo isset($img_path) ? $img_path : '../uploads/no_chosen_img.png'; ?>" alt="Vista previa de la imagen" style="max-width: 150px; max-height: 150px; margin-top: 10px;">
-
+            <input type="file" name="imagen" id="imagen_src" onchange="previewImage()">
+            <?php if (isset($img_path) && !empty($img_path)) { ?>
+                <p>Imagen actual: <?php echo basename($img_path); ?></p>
+                <img id="image-preview" src="<?php echo $img_path; ?>" alt="Vista previa de la imagen" style="max-width: 150px; max-height: 150px; margin-top: 10px;">
+            <?php } else { ?>
+                <p>No se ha seleccionado ninguna imagen.</p>
+                <img id="image-preview" src="../uploads/no_chosen_img.png" alt="Vista previa de la imagen predeterminada" style="max-width: 150px; max-height: 150px; margin-top: 10px;">
+            <?php } ?>
         </div>
 
         <input type="submit" value="<?php echo isset($edit_jacket_id) ? 'Guardar cambios' : 'Añadir'; ?>">
@@ -179,7 +190,7 @@
                 // Read file content as a data URL
                 reader.readAsDataURL(fileInput.files[0]);
             } else {
-                // If no file is selected, shows empty preview
+                // If no file is selected, show empty preview
                 imagePreview.src = '';
             }
         }
